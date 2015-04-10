6b4ee69f208d6c3294f8f405a617cf27ab39926c
Maicon Ramos <maiconx@outlook.com>
2015-04-10 16:20:30 -0300

    teste

---
+++ README.txt
@@ -0,0 +1,23 @@
+Utilizei o Xamp para fazer requisição do Less.
+
+Utilizei LESS no CSS, utilizei alguns efeito gradient e shadow para dar uma estilizada na tabela e no titulo.
+
+Dentro do Javascript : 
+
+Criei um button para chamar uma função: inserir(), que dentro dessa função faz a chamada para as funções :
+
+recebeValores() - recuperar os dados do exercicios (hora, modalidade, data)
+
+formataData() - formarta a data para o padrão brasileiro DD/MM/YYYY
+
+inserirLinha() - Inserir a linha na tabela com os dados é um button para deleta a linha
+
+somaTotal() - Recebe o paramentro de horas de exercicio e converte para inteiro depois e adicionado a um contador para retorna o total de hora da tabela.
+
+MostrarTotal() - Atualizar o valor total de exercicios tanto os que foi adicionado ou removido da tabela.
+
+Na quarta coluna da tabela tem um botão de remover com duas chamadas de função:
+
+delLinha() - Remove a linha correspodente ao botão
+
+subtrair() - Subtrai o valor de horas da linha do botão e atualizar.	
--- js/script.js
+++ js/script.js
@@ -1,34 +1,51 @@
-var total = 0;
-var celula;
-var button = "<a id='btndel' onclick='delLinha(this.parentNode.parentNode.rowIndex)'><img src='../img/btndel.png'></a>";
-
+var celula, contador = 0, total = 0;
+var button = "<a id='btndel' onclick='subtrair(this.parentNode.parentNode.cells[0].textContent);delLinha(this.parentNode.parentNode.rowIndex);'><img src='../img/btndel.png'></a>";
 function inserir() {
-    var hora = document.getElementById("horas").value;
-    var modalidade = document.getElementById("modalidade").value;
-    var dataString = document.getElementById("data").value;
-    var res = dataString.split("-");
-    var novadata = res[2] + "/" + res[1] + "/" + res[0];
+    var dados = recebeValores();
+    dados[2] = formatarData(dados[2]);
+    inserirLinha(dados[0], dados[1], dados[2]);
+    var total = somaTotal(dados[0]);
+    mostrarTotal(total);
+}
+function mostrarTotal(total) {
+    total = total;
+    document.getElementById('resultado').innerHTML = "Você já fez " + total + " horas de exercícios";
+}
+function inserirLinha(hora, modalidade, data) {
     var tabela = document.getElementById("tabela");
-
     linha = tabela.insertRow(2);
     celula = linha.insertCell(0);
     celula.innerHTML = hora + " H";
     celula = linha.insertCell(1);
     celula.innerHTML = modalidade;
     celula = linha.insertCell(2);
-    celula.innerHTML = novadata;
+    celula.innerHTML = data;
     celula = linha.insertCell(3);
     celula.innerHTML = button;
-    
-    var soma = parseInt(hora,36);
-    somaTotal(soma);
-    document.getElementById('resultado').innerHTML = "Você já fez " + total + " horas de exercícios";
 }
-
 function somaTotal(valor) {
-    total += valor;
+    valor = parseInt(valor, 10);
+    contador += valor;
+    return contador;
 }
-
 function delLinha(i) {
     tabela.deleteRow(i);
+}
+function formatarData(i) {
+    var res = i.split("-");
+    var novadata = res[2] + "/" + res[1] + "/" + res[0];
+    return novadata;
+}
+function recebeValores() {
+    var hora = document.getElementById("horas").value;
+    var modalidade = document.getElementById("modalidade").value;
+    var dataString = document.getElementById("data").value;
+    var dados = [ hora,  modalidade, dataString ];
+    return dados;
+}
+function subtrair(i) {
+    menos = i.split("H");
+    menos = parseInt(menos, 10);
+    contador = contador - menos;
+    mostrarTotal(contador);
 }
